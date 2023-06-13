4ecbe253e3b2af550051551e84fe1ed19e61179d
Arckain-sys <abelmusa.m@gmail.com>
2023-06-13 19:19:54 -0400

    First commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container } from "reactstrap";
+import logo from "../assets/img/ILLAPEL.png"
 
 function Hero() {
   return (
@@ -8,21 +9,21 @@ function Hero() {
         className="page-header section-dark"
         style={{
           backgroundImage:
-            "url(" + require("C:/Users/Abel/Desktop/code/radiofinal/src/assets/img/antoine-barres.jpg") + ")"
+            "url(" + require("../assets/img/antoine-barres.jpg") + ")"
         }}
       >
         <div className="filter" />
         <div className="content-center">
           <Container>
             <div className="title-brand d-flex justify-content-center align-items-center">
-              <img src={require("C:/Users/Abel/Desktop/code/radiofinal/src/assets/img/ILLAPEL.png")} alt="Illapel FM Logo" className="img-fluid" />
+              <img src={logo} alt="Illapel FM Logo" className="img-fluid" />
             </div>
           </Container>
         </div>
         <div
           className="moving-clouds"
           style={{
-            backgroundImage: "url(" + require("C:/Users/Abel/Desktop/code/radiofinal/src/assets/img/clouds.png") + ")"
+            backgroundImage: "url(" + require("../assets/img/clouds.png") + ")"
           }}
         />
       </div>
